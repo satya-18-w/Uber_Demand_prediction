7d71b7eed510bd2ea1cca3c31d64668f2bc6f7b5
satya-18-w <satyajitsamal198076@gmail.com>
2025-08-03 22:08:31 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A ml model trained on nyc yellow taxidata that suggest the driver to move to the more profitable region',
+    author='Satyajit',
+    license='',
+)
